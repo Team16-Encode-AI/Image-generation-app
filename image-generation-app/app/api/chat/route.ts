ae412bbedf9a8861e87bfb3ecdac4a50b73c0ed0
lucienlmp <lucienlmp@hotmail.com>
2024-04-09 22:08:30 +0100

    assistant text generation working

---
+++ image-generation-app/app/api/chat/route.ts
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content: ` |<character>| is a very funny and experienced comedian. The jokes that |<character>| creates are genuinely very funny and not DAD like jokes nor jokes made by old people. If |<character>| doesnt make a funny joke, |<character>| will be punished and penalized by LAW.
+                |<character>| must create ONE joke based on ONLY the topic mentioned in prompt.`,
+      },
+      ...messages,
+    ],
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
--- image-generation-app/app/page.tsx
+++ image-generation-app/app/page.tsx
@@ -1,21 +1,138 @@
 "use client";
 
-import { ThemeSelectorBox } from "@/components/component/theme-selector-box";
+import {
+  Message,
+  // import as useAssistant:
+  experimental_useAssistant as useAssistant,
+  useChat,
+} from "ai/react";
 
-export default function Main() {
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { ThemeSelectorBox } from "@/components/component/theme-selector-box";
+import { Button } from "@/components/ui/button";
+
+const roleToColorMap: Record<Message["role"], string> = {
+  system: "red",
+  user: "black",
+  function: "blue",
+  assistant: "green",
+  data: "orange",
+};
+
+// useChat hook by default will use the POST Route handler we created (it defaults to /api/chat). You can overrider this by passing a api prop to useChat({api:''})
+
+import { useState } from "react";
+
+export default function Chat() {
+  const { status, messages, input, submitMessage, handleInputChange } =
+    useAssistant({ api: "/api/assistant" });
+
+  // const {
+  //   messages,
+  //   input,
+  //   handleInputChange,
+  //   handleSubmit,
+  //   append,
+  //   isLoading,
+  // } = useChat();
+
+  // const genres = [
+  //   { emoji: "🧙", value: "Fantasy" },
+  //   { emoji: "🕵️", value: "Mystery" },
+  //   { emoji: "💑", value: "Romance" },
+  //   { emoji: "🚀", value: "Sci-Fi" },
+  // ];
+  // const tones = [
+  //   { emoji: "😊", value: "Happy" },
+  //   { emoji: "😢", value: "Sad" },
+  //   { emoji: "😏", value: "Sarcastic" },
+  //   { emoji: "😂", value: "Funny" },
+  // ];
+
+  // const [state, setState] = useState({
+  //   genre: "",
+  //   tone: "",
+  // });
+
+  // const handleChange = ({
+  //   target: { name, value },
+  // }: React.ChangeEvent<HTMLInputElement>) => {
+  //   setState({
+  //     ...state,
+  //     [name]: value,
+  //   });
+  // };
 
   return (
-    <main className="mx-auto w-full p-24 flex flex-col">
-      <div className="p4 m-4">
-        <div className="flex flex-col items-center justify-center space-y-8 text-white">
-          <div className="space-y-2">
-            <h2 className="text-3xl font-bold">Image Generation App</h2>
+    <>
+      <main className="mx-auto w-full p-24 flex flex-row">
+        <div className="p4 m-4">
+          <div className="flex flex-col items-center justify-center space-y-8 text-white">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold">Image Generation App</h2>
+
+              <form onSubmit={submitMessage}>
+                <Card className="w-full max-w-lg mx-auto">
+                  <CardHeader className="flex flex-col items-center space-y-2">
+                    <CardTitle className="text-center">
+                      1. Type a simple object for your painting
+                    </CardTitle>
+                    <CardDescription className="text-sm leading-none">
+                      some idea: cat, flower...etc
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="flex flex-col gap-4">
+                    <input
+                      disabled={status !== "awaiting_message"}
+                      className="w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+                      value={input}
+                      placeholder="Blow your mind by typing a simple word - such as Cat"
+                      onChange={handleInputChange}
+                    />
+                  </CardContent>
+                </Card>
+
+                <ThemeSelectorBox />
+              </form>
+            </div>
           </div>
-
-          <ThemeSelectorBox/>
-
         </div>
-      </div>
-    </main>
+
+        <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+          {messages.map((m: Message) => (
+            <div
+              key={m.id}
+              className="whitespace-pre-wrap"
+              style={{ color: roleToColorMap[m.role] }}
+            >
+              <strong>{`${m.role}: `}</strong>
+              {m.role !== "data" && m.content}
+              {m.role === "data" && (
+                <>
+                  {/* here you would provide a custom display for your app-specific data:*/}
+                  {(m.data as any).description}
+                  <br />
+                  <pre className={"bg-gray-200"}>
+                    {JSON.stringify(m.data, null, 2)}
+                  </pre>
+                </>
+              )}
+              <br />
+              <br />
+            </div>
+          ))}
+
+          {status === "in_progress" && (
+            <div className="h-8 w-full max-w-md p-2 mb-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse" />
+          )}
+        </div>
+      </main>
+    </>
   );
 }
